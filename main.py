ea648d686dbaca41fbe6b393da2073cfd2f68c08
Abdullah Faiz <abdullahfaiz@Abdullahs-MacBook-Air.local>
2026-03-03 15:42:53 +0530

    s

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
+
 # -----------------MODELS-------------------
 class userLogin(BaseModel):
     name: str
@@ -20,6 +21,23 @@ class userRegister(BaseModel):
     password: str
 
 # -------------------ROUTES-----------------
+
+@app.put('/users/{user_id}')
+def update(user_id: int, data: userRegister):
+    db = SessionLocal()
+    existing_user = db.query(User).filter(User.id == user_id).first()
+    if existing_user is None:
+        db.close()
+        return{'message': 'No id found'}
+    existing_user.name = data.name
+    existing_user.email = data.email
+    existing_user.password = data.password
+
+    db.commit()
+    db.refresh(existing_user)
+    db.close()
+    return{'message': 'Updated successfully'}
+
 @app.get('/users')
 def user_data():
     db = SessionLocal()
